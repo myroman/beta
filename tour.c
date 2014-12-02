af05409a7f1fa2dac20d83050d3b19eb11410f40
Soumadip <stmukherjee@gmail.com>
2014-12-02 15:23:19 -0500

    Added stubs for functions to implement. Will start coding after 535 demos

--- tour.c
+++ tour.c
@@ -2,8 +2,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+int createRawSockets(){
+	//Create two IP raw sockets
+	//one called rt for route traversal
+
+	//TODO: create rt raw socket
+
+	//one called pg for pinging
+
+	//TODO: create pg raw socket
+
+
+	return 0;
+}
+
+int createPfPacket(){
+	//Create a PF_SPACKET socket that should be of type SOCK_RAW
+	//Protocol value of ETH_P_IP 0x800
+	
+	//TODO: look at above note
+
+	return 0;
+}
+
+int createUDPSocket(){
+	//Create a UDP socket fro multicasting 
+	//May need two UDP sockets look at page 576
+	return 0;
+}
+
 int main(int argc, char ** argv){
-	debug("Hello World");
-	info("Howdy");
+	
+
+	//
+
+
+	createRawSockets();
+	createPfPacket();
+	createUDPSocket();
+
+
+
+
+
 	return 0;
 }
